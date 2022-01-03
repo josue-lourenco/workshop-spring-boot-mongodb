61cd55e821c8acc8ea907de39dbc9726cf9d001b
Josue Lourenco <josue_lourenco@hotmail.com>
2022-01-03 16:03:19 -0300

    Correcao Obtendo um usuario por Id

--- src/main/java/com/example/demo/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/com/example/demo/domain/User.java
@@ -1,7 +1,6 @@
 package com.example.demo.com.example.demo.domain;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -53,7 +52,10 @@ public class User implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -65,7 +67,14 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
-	}	
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+
 
 }
