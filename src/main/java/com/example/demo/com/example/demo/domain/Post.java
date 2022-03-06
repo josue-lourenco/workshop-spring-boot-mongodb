493e0cf7bbf6cfd54f70f96c95c12274c1f92f08
Josue Lourenco <josue_lourenco@hotmail.com>
2022-03-06 19:11:27 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/example/demo/com/example/demo/domain/Post.java
+++ src/main/java/com/example/demo/com/example/demo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.example.demo.com.example.demo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 
 import com.example.demo.com.example.demo.dto.AuthorDTO;
+import com.example.demo.com.example.demo.dto.CommentDTO;
 
 public class Post implements Serializable{
 	
@@ -17,6 +20,8 @@ public class Post implements Serializable{
 	private String title; 
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -71,6 +76,14 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+		
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -95,5 +108,5 @@ public class Post implements Serializable{
 			return false;
 		return true;
 	}
-		
+
 }
